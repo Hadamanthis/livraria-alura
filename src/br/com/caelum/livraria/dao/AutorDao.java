247b5902187b2c8fb94f55b5b1d019e3e1935d06
Geovane Menezes <geovane.menezes@seati.ma.gov.br>
2019-03-29 17:58:21 -0300

    Commit inicial

---
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,45 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+
+	private Banco banco = new Banco();
+
+	public void salva(Autor autor) {
+		
+		System.out.println("[INFO] Salvando o Autor " + autor.getNome());
+		
+		try {
+			Thread.sleep(20000); // 20 segundos
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		banco.save(autor);
+		
+		System.out.println("[INFO] Salvou o Autor " + autor.getNome());
+			
+	}
+	
+	public List<Autor> todosAutores() {
+		return banco.listaAutores();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.banco.buscaPelaId(autorId);
+		return autor;
+	}
+	
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println("[INFO] AutorDao foi criado.");
+	}
+	
+}
